cc4dbe86b94c689dd78a51ef5864b2bc20e6fb89
Felipe Cardoso <101072783+fcms14@users.noreply.github.com>
2024-05-06 10:17:36 -0300

    feat: input masks

--- src/components/atoms/Input/index.tsx
+++ src/components/atoms/Input/index.tsx
@@ -1,17 +1,41 @@
 import InputStyle, { InputSpanStyle, LabelStyle } from "./InputStyle"
 import { InputProps } from "../../../interfaces"
+import { useFormikContext } from "formik"
+import { BaseSyntheticEvent } from "react"
+import { Mask, MaskType } from "../../../helpers/Mask"
 
 interface Interface {
     label: string
     type: string
+    mask: MaskType
+    inputMode?: string
+    onChange?: () => void
     name: string
-    inputStyle: InputProps
+    inputStyle?: InputProps
 }
 
-const Input = ({ label, type, name, inputStyle }: Interface) => {
+const Input = ({ label, type, mask, name, inputStyle, onChange, inputMode }: Interface) => {
+    const { setFieldValue } = useFormikContext()
+
+    function handleChange(e: BaseSyntheticEvent) {
+        setFieldValue(e.currentTarget.name, Mask[mask](e.currentTarget.value))
+    }
+
     return (
         <InputSpanStyle>
-            <InputStyle {...inputStyle} placeholder={label} id={name} name={name} type={type}  />
+            <InputStyle
+                {...inputStyle}
+                placeholder={label}
+                id={name}
+                name={name}
+                type={type}
+                inputMode={inputMode}
+                onKeyUp={(e: BaseSyntheticEvent) => {
+                    handleChange(e);
+                    onChange?.()
+                }}
+                onKeyDown={handleChange}
+            />
             <LabelStyle htmlFor={name}> {label} </LabelStyle>
         </InputSpanStyle>
     )
