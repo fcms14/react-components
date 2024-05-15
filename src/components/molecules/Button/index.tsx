f7f44d3bf88c0223fe415d4760ca85f11d29adb7
Felipe Cardoso <101072783+fcms14@users.noreply.github.com>
2024-05-15 11:26:17 -0300

    rollback: molecule-button

--- src/components/molecules/Button/index.tsx
+++ src/components/molecules/Button/index.tsx
@@ -1,13 +1,9 @@
 import ButtonRoot from "./ButtonRoot";
-import ButtonIcon from "./ButtonIcon";
-import Text from "../../atoms/Text";
 import ButtonTitle from "./ButtonTitle";
-import Subtitle from "../../atoms/Subtitle";
+import ButtonIcon from "./ButtonIcon";
 
 export const Button = {
   Root: ButtonRoot,
-  Icon: ButtonIcon,
   Title: ButtonTitle,
-  Subtitle: Subtitle,
-  Text: Text
+  Icon: ButtonIcon,
 }
