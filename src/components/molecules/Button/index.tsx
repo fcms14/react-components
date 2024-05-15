d2379638db727a7724ca03b2950f70669b37e797
Felipe Cardoso <101072783+fcms14@users.noreply.github.com>
2024-05-15 09:20:03 -0300

    sync

--- src/components/molecules/Button/index.tsx
+++ src/components/molecules/Button/index.tsx
@@ -2,12 +2,12 @@ import ButtonRoot from "./ButtonRoot";
 import ButtonIcon from "./ButtonIcon";
 import Text from "../../atoms/Text";
 import Title from "../../atoms/Title";
-import Caption from "../../atoms/Caption";
+import Subtitle from "../../atoms/Subtitle";
 
 export const Button = {
   Root: ButtonRoot,
   Icon: ButtonIcon,
   Title: Title,
-  Subtitle: Caption,
+  Subtitle: Subtitle,
   Text: Text
 }
