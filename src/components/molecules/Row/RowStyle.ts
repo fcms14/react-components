27f7e4c984fe7a5a8365a3d511073ecbff12ac3c
Felipe Cardoso <101072783+fcms14@users.noreply.github.com>
2024-05-29 17:48:47 -0300

    lint

--- src/components/molecules/Row/RowStyle.ts
+++ src/components/molecules/Row/RowStyle.ts
@@ -5,17 +5,17 @@ const RowStyle = styled.div<RowProps>`
   display: flex;
 
   ${props => css`
-    align-items: ${props?.alignItems ?? "center"};  
+    align-items: ${props?.alignItems ?? "center"};
     justify-content: ${props?.justifyContent ?? "space-between"};
     
-    flex-direction: ${props?.flexDirection};    
+    flex-direction: ${props?.flexDirection};
     background-color: ${props?.backgroundColor};
     border-bottom: ${props?.borderBottom};
     border-radius: ${props?.borderRadius};
     box-shadow: ${props?.boxShadow};
     position: ${props?.position};
     top: ${props?.top};
-    margin-bottom: ${props?.marginBottom};    
+    margin-bottom: ${props?.marginBottom};
   `}
 
   ${({ theme }) => css`
