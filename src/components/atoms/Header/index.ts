b7e4ed979812b3fda262bdbcd0535c9e558dd3e0
Felipe Cardoso <101072783+fcms14@users.noreply.github.com>
2024-05-29 11:45:03 -0300

    feat: header props

--- src/components/atoms/Header/index.ts
+++ src/components/atoms/Header/index.ts
@@ -3,13 +3,13 @@ import { HeaderProps } from "../../../interfaces";
 
 const Header = styled.section<HeaderProps>`
   display: flex;
-  overflow: hidden;
-  height: 230px;
+  overflow: hidden;  
   justify-content: center;
   align-items: "center";
   
-  ${({ theme }) => css` 
-    padding: ${theme.padding.guest};
+  ${({ theme, height }) => css` 
+    height: ${height ?? "clamp(60px, 120px + 15vh, 50vh)"};
+    padding: ${height ? "clamp(${theme.padding.guest}, 20px - 3vw, 20px)" : theme.padding.header};
     background:  ${theme.colors.header.fill}};
   `}
 `
