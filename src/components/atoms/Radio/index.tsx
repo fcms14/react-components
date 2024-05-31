a10cb4e5b9efeff386de75dc7d6e319560a5d9ee
Felipe Cardoso <101072783+fcms14@users.noreply.github.com>
2024-05-31 13:38:35 -0300

    refactor: radio - missing color prop

--- src/components/atoms/Radio/index.tsx
+++ src/components/atoms/Radio/index.tsx
@@ -4,30 +4,30 @@ import RadioStyle from "./RadioStyle"
 import { BaseSyntheticEvent } from "react"
 
 interface Interface {
-    id: string
-    name: string
-    value: string
-    label: string
-    color?: string
+  id: string
+  name: string
+  value: string
+  label: string
+  color?: string
 }
 
 const Radio = ({ id, name, value, label, color }: Interface) => {
-    const { setFieldValue } = useFormikContext()
+  const { setFieldValue } = useFormikContext()
 
-    return (
-        <RadioStyle>
-            <Field
-                id={id}
-                name={name}
-                value={value}
-                type="radio"
-                onChange={(e: BaseSyntheticEvent) => setFieldValue(e.currentTarget.name, value)}
-            />
-            <label htmlFor={id}>
-                <Subtitle color={color}> {label} </Subtitle>
-            </label>
-        </RadioStyle>
-    )
+  return (
+    <RadioStyle color={color}>
+      <Field
+        id={id}
+        name={name}
+        value={value}
+        type="radio"
+        onChange={(e: BaseSyntheticEvent) => setFieldValue(e.currentTarget.name, value)}
+      />
+      <label htmlFor={id}>
+        <Subtitle color={color}> {label} </Subtitle>
+      </label>
+    </RadioStyle>
+  )
 }
 
 export default Radio
