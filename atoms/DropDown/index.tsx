e642d86145c397d1ecc3abf8dcff57be2052bcb6
Felipe Cardoso <101072783+fcms14@users.noreply.github.com>
2024-06-10 17:54:29 -0300

    feat: dropdonw wrapper -> position relative

--- atoms/DropDown/index.tsx
+++ atoms/DropDown/index.tsx
@@ -1,15 +1,19 @@
 import { IconType } from "react-icons"
-import DropDownStyle, { DropDownItemStyle } from "./DropDownStyle"
+import DropDownStyle, { DropDownItemStyle, DropDownWrapper } from "./DropDownStyle"
 import Icon from "../Icon"
 import Text from "../Text"
 import { theme } from "../../../providers/theme"
+import { useState } from "react"
+import { SlOptionsVertical } from "react-icons/sl";
+import { Button } from "../../molecules/Button"
+
 
 export interface DropDownItemInterface {
   text: string,
-  icon: IconType,
+  icon?: IconType,
   onClick?: () => void,
-  width?: number,
   color?: string
+  width?: number,
 }
 
 export interface DropDownInterface {
@@ -17,19 +21,28 @@ export interface DropDownInterface {
 }
 
 const DropDown = ({ items }: DropDownInterface) => {
+  const [showOptions, setShowOptions] = useState<boolean>(false)
+
   return (
-    <DropDownStyle>
-      {items.map(({ text, icon, onClick, color, width }: DropDownItemInterface, index: number) =>
-        <DropDownItemStyle onClick={onClick} cursor={onClick ? 'pointer' : 'inherit'} key={index}>
-          <Icon
-            icon={icon}
-            color={color ?? theme.colors.main.font}
-            width={width ?? Number(theme.fontsizes.title.default.match(/\d+/))}
-          />
-          <Text color={color ?? theme.colors.main.font}>{text}</Text>
-        </DropDownItemStyle>
-      )}
-    </DropDownStyle>
+    <DropDownWrapper onMouseLeave={() => setShowOptions(false)}>
+      <Button.Icon icon={SlOptionsVertical} onClick={() => setShowOptions(!showOptions)} color={theme.colors.main.font} applyPadding />
+
+      {showOptions &&
+        <DropDownStyle>
+          {items.map(({ text, icon, onClick, color, width }: DropDownItemInterface, index: number) =>
+            <DropDownItemStyle onClick={onClick} cursor={onClick ? 'pointer' : 'inherit'} key={index}>
+              {icon && <Icon
+                icon={icon}
+                color={color ?? theme.colors.main.font}
+                width={width ?? Number(theme.fontsizes.title.default.match(/\d+/))}
+              />}
+              <Text color={color ?? theme.colors.main.font}>{text}</Text>
+            </DropDownItemStyle>
+          )}
+        </DropDownStyle>
+      }
+
+    </DropDownWrapper>
   )
 }
 
--- organisms/Row/ContactsList.tsx
+++ organisms/Row/ContactsList.tsx
@@ -1,35 +1,33 @@
 import { Row } from "../../molecules/Row"
 import { InputMask } from "../../../components/helpers/Mask"
 import { MdPerson } from "react-icons/md"
-import DropDown, {DropDownItemInterface } from "../../atoms/DropDown"
+import DropDown, { DropDownItemInterface } from "../../atoms/DropDown"
+import { theme } from "../../../providers/theme"
 
 export interface ContactsListInterface {
-    title: string,
-    text: string,
-    actions?: DropDownItemInterface[]     
-}
-  
-export interface Interface {
-    items: ContactsListInterface[]
+  title: string,
+  text: string,
+  actions?: DropDownItemInterface[]
 }
 
-const ContactsList = ({items}: Interface) => {
-    return (
-        <>
-        {items.map((item, key:number) => (
-            <Row.Root key={key} rowStyle={{ borderBottom: `1px solid` }}>
-                <Row.Icon icon={MdPerson} rounded />
-                <Row.Section>
-                    <Row.Title size="small"> {InputMask.name(item.title)} </Row.Title>
-                    <Row.Text> {item.text} </Row.Text>
-                </Row.Section>
-                {item.actions &&  
-                <DropDown items={item.actions}/>
-                }
-            </Row.Root>                     
-        ))}
+export interface Interface {
+  items: ContactsListInterface[]
+}
+
+const ContactsList = ({ items }: Interface) => {
+  return (
+    <>
+      {items.map((item, key: number) => (
+        <Row.Root key={key} rowStyle={{ borderBottom: `1px solid ${theme.colors.main.stroke}` }}>
+          <Row.Icon icon={MdPerson} rounded />
+          <Row.Section>
+            <Row.Title size="small"> {InputMask.name(item.title)} </Row.Title>
+            <Row.Text> {item.text} </Row.Text>
+          </Row.Section>
+          {item.actions && <DropDown items={item.actions} />}
+        </Row.Root>
+      ))}
     </>
-    )                      
-      
+  )
 }
 export default ContactsList
