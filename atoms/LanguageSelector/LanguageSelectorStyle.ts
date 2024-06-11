0372ad6e43c4a813ad37a6f67346e67415e8bff1
Felipe Cardoso <101072783+fcms14@users.noreply.github.com>
2024-06-11 13:52:21 -0300

    feat: language selector opacity

--- atoms/LanguageSelector/LanguageSelectorStyle.ts
+++ atoms/LanguageSelector/LanguageSelectorStyle.ts
@@ -3,12 +3,19 @@ import styled, { css } from "styled-components"
 const LanguageSelectorStyle = styled.div`
     display: flex;
     position: fixed;
-
+    z-index: 1;
+    
     ${({ theme }) => css`
         gap: ${theme.gap};
         bottom: ${theme.gap};
         right: ${theme.gap};
+        background-color: ${theme.colorDefault.white};
+        opacity: 0.2;
     `}
+
+    :hover {
+        opacity: 1
+    }
 
     > span {
         cursor: pointer;
