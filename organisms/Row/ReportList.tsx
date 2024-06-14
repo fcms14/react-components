9ed1b5b798f07a8cd9ab5fa904a19babe86e3c28
Felipe Cardoso <101072783+fcms14@users.noreply.github.com>
2024-06-14 15:46:36 -0300

    refactor: ReportList

--- organisms/Row/ReportList.tsx
+++ organisms/Row/ReportList.tsx
@@ -1,5 +1,4 @@
 import { Row } from "../../molecules/Row"
-import { InputMask } from "../../../components/helpers/Mask"
 import { MdNavigateNext, MdPerson } from "react-icons/md"
 import DropDown, { DropDownItemInterface } from "../../atoms/DropDown"
 import { theme } from "../../../providers/theme"
@@ -17,17 +16,17 @@ export interface Interface {
 const ReportList = ({ items }: Interface) => {
   return (
     <>
-      {items.map((item, key: number) => (
+      {items.map(({ text, title, actions }, key: number) => (
         <Row.Root key={key} rowStyle={{ borderBottom: `1px solid ${theme.colors.main.stroke}` }}>
           <Row.Icon icon={MdPerson} rounded />
           <Row.Section>
-            <Row.Title size="small"> {InputMask.name(item.title)} </Row.Title>
-            <Row.Text> {item.text} </Row.Text>
+            <Row.Title size="small"> {title} </Row.Title>
+            <Row.Text> {text} </Row.Text>
           </Row.Section>
-          {item.actions ?
-            item.actions.length === 1
-              ? <Row.Icon icon={MdNavigateNext} color={theme.colors.main.font} onClick={item.actions[0].onClick} width={Number(theme.fontsizes.title.big.match(/\d+/))} />
-              : <DropDown items={item.actions} />
+          {actions ?
+            actions.length === 1
+              ? <Row.Icon icon={actions[0].icon ?? MdNavigateNext} color={theme.colors.main.font} onClick={actions[0].onClick} width={Number(theme.fontsizes.title.big.match(/\d+/))} />
+              : <DropDown items={actions} />
             : <></>
           }
         </Row.Root>
