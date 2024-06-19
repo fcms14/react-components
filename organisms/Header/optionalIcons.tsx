a66cd8269532ab1514ae2e3b7185d2404a4a837d
Felipe Cardoso <101072783+fcms14@users.noreply.github.com>
2024-06-19 10:29:24 -0300

    feat: default icon size for header optional icons

--- organisms/Header/optionalIcons.tsx
+++ organisms/Header/optionalIcons.tsx
@@ -3,22 +3,23 @@ import { HeaderProps } from "../../atoms/Header";
 import { theme } from "../../../providers/theme";
 import { Header } from "../../molecules/Header"
 
-interface Interface {
+export interface HeaderOptionalIconsInterface {
   text: string,
   leftIcon?: IconComponentInterface
   rightIcon?: IconComponentInterface
   iconBefore?: boolean
 }
 
-const HeaderOptionalIcons = ({ text, leftIcon, rightIcon, iconBefore }: Interface) => {
-  const size = `${(leftIcon?.width ?? rightIcon?.width ?? 16) + (Number(theme.padding.icon.match(/\d+/)) * 2)}px`;
+const HeaderOptionalIcons = ({ text, leftIcon, rightIcon, iconBefore }: HeaderOptionalIconsInterface) => {
+  const iconWidth = leftIcon?.width ?? rightIcon?.width ?? Number(theme.fontsizes.title.big.match(/\d+/))
+  const size = `${(iconWidth) + (Number(theme.padding.icon.match(/\d+/)) * 2)}px`;
   const headerStyle: HeaderProps | undefined = iconBefore ? { flexDirection: "row-reverse" } : undefined
 
   return (
     <Header.Root headerStyle={{ ...headerStyle }}>
-      {leftIcon ? <Header.Icon {...leftIcon} /> : <Header.Spacer width={size} />}
+      {leftIcon ? <Header.Icon {...leftIcon} width={iconWidth} /> : <Header.Spacer width={size} />}
       <Header.Title parent="header"> {text} </Header.Title>
-      {rightIcon ? <Header.Icon {...rightIcon} /> : <Header.Spacer width={size} />}
+      {rightIcon ? <Header.Icon {...rightIcon} width={iconWidth} /> : <Header.Spacer width={size} />}
     </Header.Root>
   )
 }
