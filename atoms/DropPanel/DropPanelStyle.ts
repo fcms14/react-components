efd63742b8e9b1806a7bd9b0dba693c3fcc18d3f
Felipe Cardoso <101072783+fcms14@users.noreply.github.com>
2024-07-01 19:15:09 -0300

    feat: dropPanel border radius

--- atoms/DropPanel/DropPanelStyle.ts
+++ atoms/DropPanel/DropPanelStyle.ts
@@ -11,6 +11,7 @@ const DropPanelStyle = styled.div`
   box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.25);
   background-color: ${({ theme }) => theme.colorDefault.white};
   gap: ${({ theme }) => theme.gap};
+  border-radius: ${({ theme }) => theme.radius};
 `
 
 interface Props {
