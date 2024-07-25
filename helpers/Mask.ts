151ecce8c5558cbc09e428e3b6911cd5b969f8bf
Pedro <pedro.negreiros94@hotmail.com>
2024-07-25 14:53:10 -0300

    feat: unmaskerPhone

--- helpers/Mask.ts
+++ helpers/Mask.ts
@@ -35,7 +35,8 @@ export const configOptions = {
 export const Parser = {
     intToFloat: (value: number | string): number => Number(value) / 100,
     unmasker: (value: string, replace: string = "R$"): number => Number(value.replace(replace, "").replaceAll('.', '').replace(',', '.')),
-    unmaskerToInt: (value: string): number => Number(value.replace("R$", "").replace(/[^\w\s]/gi, '').replaceAll(' ', ''))
+    unmaskerToInt: (value: string): number => Number(value.replace("R$", "").replace(/[^\w\s]/gi, '').replaceAll(' ', '')),
+    unmaskerPhone: (value: string): string => value.replace(/[()\-]/g, '')
 }
 
 export const Mask = {
