04fb3bca96dfa66165c2d0a129f730a7d8746f37
Felipe Cardoso <felipe.cardoso@uello.com.br>
2024-10-17 09:06:47 -0300

    fix: RowCopy file location -> molecule to organism

--- organisms/Row/index.tsx
+++ organisms/Row/index.tsx
@@ -5,12 +5,14 @@ import ReportList from "./ReportList";
 import RowList from "./RowList"
 import RowTitleAndLink from "./RowTitleAndLink";
 import RowBox from "./RowBox";
+import RowCopy from "./RowCopy";
 
 export const Row = {
     Card: RowCard,
     Box: RowBox,
     FourColumnRow: FourColumnRow,
     Menu: MenuList,
+    Copy: RowCopy,
     ReportList: ReportList,
     List: RowList,
     TitleAndLink: RowTitleAndLink,
