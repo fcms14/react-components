31abf23c6995635e24845f256cc9aabed7371863
Felipe Cardoso <felipe.cardoso@uello.com.br>
2024-10-17 13:11:16 -0300

    fix: rollback input

--- atoms/Input/index.tsx
+++ atoms/Input/index.tsx
@@ -8,7 +8,6 @@ import { theme } from "../../../providers/theme"
 import { MdRemoveRedEye } from "react-icons/md"
 import { FaEyeSlash } from "react-icons/fa"
 import { useTranslation } from "react-i18next"
-import PasswordValidator from "../../molecules/PasswordValidator"
 
 export interface InputInterface {
     name: string
@@ -23,57 +22,52 @@ export interface InputInterface {
     error?: string
     icon?: IconComponentInterface
     disabled?: boolean
-    validator?: boolean
 }
 
-const Input = ({ error, label, type, mask, maskConfig, name, onChange, onFocus, onBlur, inputMode, icon, disabled, validator }: InputInterface) => {
-    const { setFieldValue } = useFormikContext()
+const Input = ({ error, label, type, mask, maskConfig, name, onChange, onFocus, onBlur, inputMode, icon, disabled }: InputInterface) => {
+    const { setFieldValue, setFieldTouched } = useFormikContext()
     const { t } = useTranslation()
     const [show, toggleVisibility] = useState<boolean>(false)
-    const [password, setPassword] = useState('');
 
     function handleChange(e: BaseSyntheticEvent, mask: MaskType) {
         if (!mask) return
+
         const config = configOptions[maskConfig ?? "default"]
         const value = InputMask[mask](e.currentTarget.value, config)
-        setPassword(value)
         setFieldValue(e.currentTarget.name, value)
         return value
     }
 
     return (
-        <>
-            <InputSpanStyle onClick={onFocus}>
-                <InputStyle
-                    placeholder={label}
-                    id={name}
-                    name={name}
-                    type={(type === "password" && show) ? "text" : type}
-                    inputMode={inputMode}
-                    onBlur={onBlur}
-                    onFocus={onFocus}
-                    onKeyUp={(e: BaseSyntheticEvent) => {
-                        const event = e as React.KeyboardEvent<HTMLInputElement>;
-                        if (event.code === "Backspace" && !validator) return
-                        const value = mask ? handleChange(e, mask) : e.currentTarget.value;
-                        setPassword(value);
-                        onChange?.(value)
-                    }}
-                    onKeyDown={mask ? handleChange : undefined}
-                    disabled={disabled}
-                />
-                <LabelStyle htmlFor={name}> {label} </LabelStyle>
-                {error && <ErrorSpanStyle> <Subtext size="big" color={theme.colorDefault.error}>{t(error ?? "")}</Subtext> </ErrorSpanStyle>}
-                {(type === "password") && <Icon icon={!show ? FaEyeSlash : MdRemoveRedEye} onClick={() => toggleVisibility(!show)} />}
-                {icon && <Icon {...icon} onClick={(e?: BaseSyntheticEvent) => {
-                    if (icon.onClick) {
-                        e?.stopPropagation()
-                        icon.onClick()
-                    }
-                }} />}
-            </InputSpanStyle>
-            {type === 'password' && validator && password != '' && <PasswordValidator password={password} />}
-        </>
+        <InputSpanStyle onClick={onFocus}>
+            <InputStyle
+                placeholder={label}
+                id={name}
+                name={name}
+                type={(type === "password" && show) ? "text" : type}
+                inputMode={inputMode}
+                onBlur={onBlur}
+                onFocus={onFocus}
+                onKeyUp={(e: BaseSyntheticEvent) => {
+                    const event = e as React.KeyboardEvent<HTMLInputElement>;
+                    if (event.code === "Backspace") return
+                    const value = mask ? handleChange(e, mask) : e.currentTarget.value;
+                    setFieldTouched(e.currentTarget.name)
+                    onChange?.(value)
+                }}
+                onKeyDown={mask ? handleChange : undefined}
+                disabled={disabled}
+            />
+            <LabelStyle htmlFor={name}> {label} </LabelStyle>
+            {error && <ErrorSpanStyle> <Subtext size="big" color={theme.colorDefault.error}>{t(error ?? "")}</Subtext> </ErrorSpanStyle>}
+            {(type === "password") && <Icon icon={!show ? FaEyeSlash : MdRemoveRedEye} onClick={() => toggleVisibility(!show)} />}
+            {icon && <Icon {...icon} onClick={(e?: BaseSyntheticEvent) => {
+                if (icon.onClick) {
+                    e?.stopPropagation()
+                    icon.onClick()
+                }
+            }} />}
+        </InputSpanStyle>
     )
 }
 
